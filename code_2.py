1f4bb60e5f4af354b8cb823d624cb9ad4fb5b0cd
Judy <143587407+Judy770@users.noreply.github.com>
2023-11-06 09:24:21 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,59 @@
+    main()
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+def main():
+if __name__ == "__main__":
+import random
+
+
+def generate_random_data():
+    return datafunction generateRandomData() {
+
+    return data;
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+main();
+    }
+
+
+}
+function main() {
+    const data = generateRandomData();
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+    });
+    data.forEach(item => {    const data = [];
+    data.forEach(item => {
+
+
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+}
+    }
+
+    return data;
+    });
+        console.log("Random Number: " + item);
+}
+main();
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {    return data
+if __name__ == "__main__":
+def generate_random_data():
+
+    data = generate_random_data()
+import random
+
+
+    for item in data:
+
+        print(f"Random Number: {item}")
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
